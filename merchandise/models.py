d325398bff3c210bff344b9eb34abba47076ed87
tarekgabarin <tarekgabarin1992@gmail.com>
2019-09-04 19:25:57 -0400

    added merchandise

--- merchandise/models.py
+++ merchandise/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Merchandise(models.Model):
+    name = models.CharField(max_length=120, default='UNKNOWN')
+    price = models.DecimalField(decimal_places=2, max_digits=10000, default=0)
+    supply = models.IntegerField(default=0)
